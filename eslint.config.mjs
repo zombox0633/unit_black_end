198d640f069f0bcdfcf7f22591e9eeea794c6dd9
Zombox <ghostzb@outlook.com>
2025-03-20 17:13:13 +0700

    edit @typescript-eslint/no-unsafe-call to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,10 +26,11 @@ export default tseslint.config(
   },
   {
     rules: {
+      '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      'prettier/prettier': ['error', { endOfLine: 'auto' }]
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 );
