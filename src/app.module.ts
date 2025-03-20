fd318b5f2b6ff688423c2e0aea34f408d329366b
Zombox <ghostzb@outlook.com>
2025-03-20 12:54:36 +0700

    add Module unit_postgres db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HotelController } from './hotel/hotel.controller';
+import { HotelService } from './hotel/hotel.service';
+import { Hotel } from './hotel/hotel.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'unit_postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+      retryAttempts: 10,
+      retryDelay: 3000,
+    }),
+    TypeOrmModule.forFeature([Hotel]),
+  ],
+  controllers: [AppController, HotelController],
+  providers: [AppService, HotelService],
 })
 export class AppModule {}
